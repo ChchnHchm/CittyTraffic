7ec028fd25097d3afba6708fceecb2d1ee869867
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2023-01-13 11:19:51 +0100

    ajout constante

--- backend/compile.sh
+++ backend/compile.sh
@@ -1,3 +1,3 @@
 !/bin/bash
-
-mvn clean; mvn compile; mvn package; scp /net/cremi/hchouchane/espaces/travail/prog\ large\ echelle/Projet/CittyTraffic/backend/target/CittyTrafficProject-0.0.1.jar hchouchane@147.210.117.54:/home/hchouchane/Project
+readonly LOGIN = hchouchane
+mvn clean; mvn compile; mvn package; scp /net/cremi/LOGIN/espaces/travail/prog\ large\ echelle/Projet/CittyTraffic/backend/target/CittyTrafficProject-0.0.1.jar LOGIN@147.210.117.54:/home/LOGIN/
