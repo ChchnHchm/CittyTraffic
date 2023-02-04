1307220096035984dd91d45655703a7fedeef714
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2023-02-04 18:53:06 +0100

    ajout des routes get

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/*
+ return all data for a specified  day
+*/
+router.get('/getDate', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/*
+ return all data for  a specified hour
+*/
+router.get('/getHours', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/*
+return all data for a radar and specified hour
+*/
+router.get('/getRadarDate', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/*
+return all data for specified hour
+*/
+router.get('/getRadarHours', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
