02b052933e68e307c8e4251958eab70d3f4f22b1
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2023-02-04 20:35:49 +0100

    modif routes get

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -2,36 +2,60 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    res.status(200);
 });
 
 /*
  return all data for a specified  day
 */
-router.get('/getDate', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getDate', async function(req, res, next) {
+  res.setHeader('Content-Type',"application/json");
+  try {
+    res.status(200).json(); //rajouter fonction
+  } catch (error) {
+    console.error(error);
+    await res.status(424).json({error});
+  }
 });
 
 /*
  return all data for  a specified hour
 */
-router.get('/getHours', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getHours', async function(req, res, next) {
+  res.setHeader('Content-Type',"application/json");
+  try {
+    res.status(200).json(); //rajouter fonction
+  } catch (error) {
+    console.error(error);
+    await res.status(424).json({error});
+  }
 });
 
 /*
 return all data for a radar and specified hour
 */
-router.get('/getRadarDate', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getRadarDate', async function(req, res, next) {
+  res.setHeader('Content-Type',"application/json");
+  try {
+    res.status(200).json(); //rajouter fonction
+  } catch (error) {
+    console.error(error);
+    await res.status(424).json({error});
+  }
 });
 
 /*
 return all data for specified hour
 */
-router.get('/getRadarHours', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getRadarHours', async function(req, res, next) {
+  res.setHeader('Content-Type',"application/json");
+  try {
+    res.status(200).json(); //rajouter fonction
+  } catch (error) {
+    console.error(error);
+    await res.status(424).json({error});
+  }
 });
 
 module.exports = router;
---
+++ frontend/src/store/cityTrafficAPI.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+const API = "http://localhost:1369/citytraffic/api/";
+
+export const CityTrafficAPI =  {
+    getByDate: async function(date){
+        const URL = API + "getDate";
+        return await  axios.get(
+            URL,{
+                params: { 
+                    date: date
+                }
+            }
+        ).then( res => res.data);
+    },
+    getByHours: async function(date,hour){
+        const URL = API + "getHours";
+        return await  axios.get(
+            URL,{
+                params: {
+                    date: date,
+                    hours:hour
+                }
+            }
+        ).then( res => res.data);
+    },
+    getByRadarDate: async function(radar,date){
+        const URL = API + "getRadarDate";
+        return await  axios.get(
+            URL,{
+                params: {
+                    radar: radar,
+                    date: date
+                }
+            }
+        ).then( res => res.data);
+    },
+    getByRadarHours: async function(radar,date,hour){
+        const URL = API + "getRadarHours";
+        return await  axios.get(
+            URL,{
+                params: {
+                    radar: radar,
+                    date: date,
+                    hour: hour
+                }
+            }
+        ).then( res => res.data);
+    }
+}
+
+export { CityTrafficAPI }; 
