7686f58dd6fa9b7f6d10ed2314ec6ede4527bde3
Nathanael Alves <nathanael.alves@etu.u-bordeaux.fr>
2023-02-05 16:41:33 +0100

    d

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -26,7 +26,7 @@ router.get('/getDate', async function(req, res, next) {
     //   console.info(error)
     //   });
     // filterFunctions.filterDate(client.table(username+':CittyTrafficHbase'),req.query.date);
-    table.row("2022-10-12,8,P4").get('direction:enter', (error, value) => {
+    table.row("2022-10-12,8,P4").get('type:vl', (error, value) => {
       console.log(value)
     });
     res.status(200).json(); //rajouter fonction
