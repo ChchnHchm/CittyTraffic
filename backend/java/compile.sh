7e6f6e917295a65a3ee9abe99808625804e82dd6
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2023-02-06 21:35:47 +0100

    ajout repertoire java et demande de login dans le compile

--- backend/java/compile.sh
+++ backend/java/compile.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
-readonly LOGIN=nalves
+read -p "Entrer votre login:" LOGIN
 mvn clean; mvn compile; mvn package; scp ./target/Exec-CittyTrafficProcessing.jar $LOGIN@147.210.117.54:/home/$LOGIN/ ;scp ./target/Exec-CittyTrafficCleaner.jar $LOGIN@147.210.117.54:/home/$LOGIN/ ;
